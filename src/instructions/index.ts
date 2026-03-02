ba4a7c6f790178557c67a1f7926e7d4e399a2672
Spencer Pearson <speeze.pearson@gmail.com>
2026-03-01 19:09:43 -0800

    add initFormationStates

--- src/instructions/index.ts
+++ src/instructions/index.ts
@@ -2,6 +2,9 @@ import { z } from 'zod';
 import { type Beats } from '../contraCore';
 import { AtomicInstructionSchema } from './_atomic';
 import { getSplitDuration, SplitSchema } from './split';
+import type { WorldState } from '../worldState';
+import { Vector } from 'vecti';
+import { EAST, NORTH, SOUTH, WEST } from '../geometry';
 
 export { InstructionIdSchema, type InstructionId, type RelativeDirection, RelativeDirectionSchema } from './_base';
 
@@ -28,6 +31,27 @@ export function danceLength(instructions: Instruction[]): Beats {
 
 export const InitFormationSchema = z.enum(['improper', 'beckett']);
 export type InitFormation = z.infer<typeof InitFormationSchema>;
+
+export const initFormationStates: Record<InitFormation, WorldState> = {
+  improper: {
+    beat: 0,
+    protos: {
+      up_lark_0: { protoId: 'up_lark_0', pos: new Vector(-0.5, -0.5), facing: NORTH, hands: {} },
+      up_robin_0: { protoId: 'up_robin_0', pos: new Vector(0.5, -0.5), facing: NORTH, hands: {} },
+      down_lark_0: { protoId: 'down_lark_0', pos: new Vector(0.5, 0.5), facing: SOUTH, hands: {} },
+      down_robin_0: { protoId: 'down_robin_0', pos: new Vector(-0.5, 0.5), facing: SOUTH, hands: {} },
+    },
+  },
+  beckett: {
+    beat: 0,
+    protos: {
+      up_lark_0: { protoId: 'up_lark_0', pos: new Vector(-0.5, 0.5), facing: EAST, hands: {} },
+      up_robin_0: { protoId: 'up_robin_0', pos: new Vector(-0.5, -0.5), facing: EAST, hands: {} },
+      down_lark_0: { protoId: 'down_lark_0', pos: new Vector(0.5, -0.5), facing: WEST, hands: {} },
+      down_robin_0: { protoId: 'down_robin_0', pos: new Vector(0.5, 0.5), facing: WEST, hands: {} },
+    },
+  },
+};
 
 export const ProgressionSchema = z.number().int();
 
