aa047100aef3da96a484df9f94f4203ec4802e58
Spencer Pearson <speeze.pearson@gmail.com>
2026-03-07 09:40:48 -0800

    fix do si do facing

--- src/instructions/doSiDo.ts
+++ src/instructions/doSiDo.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
-import { TWO_PI } from "../geometry";
+import { getDir, TWO_PI } from "../geometry";
+import { must } from "../utils";
 import { CalledIdentifierSchema, instructionBaseSchemaFields } from "./_base";
 import { arc, type InstructionAnimator } from "./_segment";
 
@@ -21,6 +22,13 @@ export const doSiDoSegments: InstructionAnimator<DoSiDoInstruction> = (
       semiMinor: 0.25,
       phi: TWO_PI * instr.rotations,
     }),
+    facing: (dancer) => {
+      const match = must(dancer.resolveCalledIdentifier(instr.cid), [
+        { dancerId: dancer.id },
+        "has no match to do si do with",
+      ]);
+      return getDir({ from: dancer.pos, to: match.pos });
+    },
     interactedWith: (dancer) => [dancer.resolveMatch(instr.cid).id],
   },
 ];
